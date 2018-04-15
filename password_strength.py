63259a3b76789694483307f57f0cebb90cc7bd51
Ubuntu <ubuntu@ip-172-31-44-106.us-east-2.compute.internal>
2018-04-15 17:52:02 +0000

    Добавил чтение файла с запрещенными словами, поправил обработку суммирования баллов, изменил метод ввода пароля

--- password_strength.py
+++ password_strength.py
@@ -1,6 +1,16 @@
 from string import punctuation
 import re
 import sys
+import getpass
+
+
+def load_file(filepath):
+    with open(filepath, 'r') as file:
+        return file.read()
+
+
+def get_blacklisted_list(loaded_file):
+    return loaded_file.split()
 
 
 def has_appropriate_len(password):
@@ -23,35 +33,41 @@ def has_special_char(password):
     return any(letter in punctuation for letter in password)
 
 
-def hasnt_blacklisted_words(password):
-    blacklist = ['password', 'mail', 'cat', 'dog']
-    return all(
-            blacklisted_word not in password.lower()
-            for blacklisted_word in blacklist
-            )
+def hasnt_blacklisted_words(password, blacklisted_words):
+    if not blacklisted_words:
+        return False
+    blacklisted_words = blacklisted_words
+    return bool(password.lower() not in blacklisted_words)
 
 
 def hasnt_date(password):
     date_pattern = re.search('\d{6,}', password)
-    return True if not date_pattern else False
+    return not bool(date_pattern)
 
 
-def get_password_strength(password):
+def get_password_strength(password, blacklisted_words):
     extra_point = 2
     score = 0
-    score += has_appropriate_len(password) * extra_point
-    score += has_numbers(password)
-    score += has_upper(password) * extra_point
-    score += has_lower(password)
-    score += has_special_char(password) * extra_point
-    score += hasnt_blacklisted_words(password)
-    score += hasnt_date(password)
+    score = sum(((has_appropriate_len(password) * extra_point),
+                has_numbers(password), (has_upper(password) * extra_point),
+                has_lower(password),
+                (has_special_char(password) * extra_point),
+                hasnt_blacklisted_words(password, blacklisted_words),
+                hasnt_date(password)))
     return score
 
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        sys.exit('Вы не ввели пароль!')
-    password = sys.argv[1]
-    score = get_password_strength(password)
+        blacklisted_words_list = None
+        print('Проверка по blacklist-справочнику проводиться не будет!')
+    else:
+        filepath = sys.argv[1]
+        try:
+            blacklisted_words_data = load_file(filepath)
+        except FileNotFoundError:
+            sys.exit('Указан неправильный путь к файлу!')
+        blacklisted_words_list = get_blacklisted_list(blacklisted_words_data)
+    password = getpass.getpass()
+    score = get_password_strength(password, blacklisted_words_list)
     print('Оценка вашего пароля: {}!'.format(score))
