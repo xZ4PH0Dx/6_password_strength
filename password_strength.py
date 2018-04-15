cd97f586dd1ca2a9cb56244f9550d10557096136
Ubuntu <ubuntu@ip-172-31-44-106.us-east-2.compute.internal>
2018-04-15 19:32:32 +0000

    Попробуем обойти проверку на вложенность..

--- password_strength.py
+++ password_strength.py
@@ -44,17 +44,13 @@ def hasnt_date(password):
 def get_password_strength(password, blacklisted_words):
     extra_point = 2
     score = 0
-    score = sum(
-                (
-                    has_appropriate_len(password) * extra_point,
-                    has_numbers(password),
-                    has_upper(password) * extra_point,
-                    has_lower(password),
-                    has_special_char(password) * extra_point,
-                    hasnt_blacklisted_words(password, blacklisted_words),
-                    hasnt_date(password)
-                )
-               )
+    score = sum((has_appropriate_len(password) * extra_point,
+                 has_numbers(password),
+                 has_upper(password) * extra_point,
+                 has_lower(password),
+                 has_special_char(password) * extra_point,
+                 hasnt_blacklisted_words(password, blacklisted_words),
+                 hasnt_date(password)))
     return score
 
 
